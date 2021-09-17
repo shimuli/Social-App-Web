da5fd866a74846ad0b5778e3e87d0ab1b276e391
shimuli <shimulicedric@gmail.com>
2021-09-17 11:42:18 +0300

    cert

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'socialapp';
+export class AppComponent implements OnInit {
+  title = 'Social App';
+  users : any;
+
+  constructor(private http: HttpClient) {
+
+  }
+  ngOnInit() {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.http.get("https://localhost:5001/api/users/getusers").subscribe(
+      response => {
+        this.users = response;
+      },
+      error => {
+        console.log(error);
+      })
+  }
 }
