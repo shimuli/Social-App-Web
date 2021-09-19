cfe3266286ccd7144972dafcb732f4dcd6c2673d
shimuli <shimulicedric@gmail.com>
2021-09-19 21:56:49 +0300

    Consume login API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,7 +18,7 @@ export class AppComponent implements OnInit {
   }
 
   getUsers() {
-    this.http.get("https://localhost:5001/api/users/getusers").subscribe(
+    this.http.get("https://localhost:5001/api/users").subscribe(
       response => {
         this.users = response;
       },
